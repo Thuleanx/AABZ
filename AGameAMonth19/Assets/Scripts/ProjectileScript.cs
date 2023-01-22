5e4ef9381a5f6d0fef2395024e79de607e21a5e3
Thuleanx <thuleanx@tutanota.com>
2023-01-22 00:19:42 -0500

    Projectile bounce off shield
    Added more layers. layer [Projectile] will collide with layer [Shield]

--- AGameAMonth19/Assets/Scripts/ProjectileScript.cs
+++ AGameAMonth19/Assets/Scripts/ProjectileScript.cs
@@ -8,6 +8,7 @@ public class ProjectileScript : MonoBehaviour
 	private Rigidbody2D _rigidbody;
 
   	// ================== Accessors
+	private Vector2 _velocityLastFrame;// need this because when collision happens, our speed is potentially decreased
 	public Vector2 Velocity { get => _rigidbody.velocity; set => _rigidbody.velocity = value; }
 	public Timer CurrentLifetime {get; private set; }
 
@@ -28,9 +29,20 @@ public class ProjectileScript : MonoBehaviour
 		}
 	}
 
+	private void LateUpdate() {
+		_velocityLastFrame = Velocity;
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision) {
+		Vector2 collisionNormal = collision.contacts[0].normal;
+		// WARNING: this will override the bullet speed. 
+		Velocity = Vector2.Reflect(_velocityLastFrame, collisionNormal);
+	}
+
 	public void Initialize(Vector2 velocity, bool rotatedToFaceDir)
 	{
 		Velocity = velocity;
+		_velocityLastFrame = Velocity;
 		if (rotatedToFaceDir) transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(velocity.y, velocity.x));
 	}
 }
