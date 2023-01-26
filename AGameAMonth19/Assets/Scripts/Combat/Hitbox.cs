80f6adf96279e9dd93e56d1adedc12d0dd6dd32b
Thuleanx <allison_yeh@brown.edu>
2023-01-25 19:55:52 -0500

    Hitbox + Hurtbox

---
+++ AGameAMonth19/Assets/Scripts/Combat/Hitbox.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Modules.Combat
+{
+	[RequireComponent(typeof(Collider2D))]
+	public class Hitbox : MonoBehaviour
+	{
+		[SerializeField] private int _damage;
+		[SerializeField] private UnityEvent<Hitbox> _onHit;
+
+		private void OnTriggerEnter2D(Collider2D other) {
+			Status stat = other.GetComponentInParent<Status>();
+			stat.TakeDamage(_damage, isHit: true);
+			_onHit?.Invoke( this );
+		}
+	}
+}
---
+++ AGameAMonth19/Assets/Scripts/Combat/Status.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Modules.Combat
+{
+	public class Status : MonoBehaviour
+	{
+		[SerializeField] private int _maxHealth = 10;
+
+		public UnityEvent<Status> OnDeath;
+		public UnityEvent<Status> OnHit;
+
+		public int Health { get; private set; }
+		public bool IsDead => Health == 0;
+
+		private void OnEnable() {
+			Health = _maxHealth;
+		}
+
+		/// <summary>
+		/// Accounts for an instance of damage. Will trigger <see cref="OnDeath"> if Health falls to zero.
+		/// </summary>
+		/// <param name="dmg"></param>
+		public void TakeDamage(int dmg, bool isHit)
+		{
+			if (Health > 0)
+			{
+				Health = Mathf.Max(Health - dmg, 0);
+				if (Health == 0)	OnDeath?.Invoke( this );
+				else if (isHit) 	OnHit?.Invoke( this );
+			}
+		}
+	}
+}
